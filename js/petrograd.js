b3b7edb35d92ce10af3d274b57117c9cff7913bf
Kristian Kromand <kristiankromand@Kristians-MacBook-Pro-2.local>
2017-02-23 13:14:08 +0100

    Færdig dag 1

--- js/petrograd.js
+++ js/petrograd.js
@@ -36,6 +36,13 @@ function visProdukt(produkt) {
         klon.querySelector(".pris").classList.add("udsolgt");
     }
 
+    if (produkt.udsolgt == true || produkt.rabatsats == 0) {
+        var rabatpris = klon.querySelector(".rabatpris");
+        rabatpris.parentNode.removeChild(rabatpris);
+    } else {
+        klon.querySelector(".pris").classList.add("gammeltext")
+
+    }
     //append klon til .produkt_liste
     document.querySelector(".produktliste").appendChild(klon);
 }
