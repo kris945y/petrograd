a2bf136c1e1a1513406e0f5136228cbd68bc5e6a
Kristian Kromand <kristiankromand@Kristians-MacBook-Pro-2.local>
2017-02-23 11:59:49 +0100

    load JSON produktliste and create template clone for each

--- js/petrograd.js
+++ js/petrograd.js
@@ -2,11 +2,20 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
+
+    //læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
     visProdukt();
 }
 
-function visProdukt() {
-    // Klon produkt_template
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt(produkt) {
+    console.log(produkt)
+        // Klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode("true");
     //indsæt data i klon
 
